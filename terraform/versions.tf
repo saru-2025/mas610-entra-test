f093d52cc857e5f8e9e9f149f0ee5cc8ff241ca7
Saritha Mpragada <saritha.mpragada@outlook.com>
2025-11-03 18:19:15 +0800

    chore: remove provider cache configuration for simplicity

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -7,14 +7,4 @@ terraform {
       version = ">= 1.33.0"
     }
   }
-
-  # Configure provider cache
-  provider_installation {
-    dev_overrides {
-      "databricks/databricks" = "${path.root}/providers"
-    }
-    direct {
-      exclude = ["databricks/databricks"]
-    }
-  }
 }
