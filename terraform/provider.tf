9e6f84d865b79ec0bb4d1c412a8e9dc8370a95da
Saritha M <saritha.inf@gmail.com>
2025-11-05 10:34:45 +0800

    Delete terraform directory

--- terraform/provider.tf
+++
@@ -1,4 +0,0 @@
-provider "databricks" {
-  host  = var.databricks_host != "" ? var.databricks_host : env("DATABRICKS_HOST")
-  token = var.databricks_token != "" ? var.databricks_token : env("DATABRICKS_TOKEN")
-}
